301d582c7ba6a4c474b810c6f13ffcb5892782a1
dhunnicuttsg <dhunnicutt527@gmail.com>
2026-09-03 13:38:01 -0500

    Updated app to output correct mimetype for metrics

--- docker/app/app.py
+++ docker/app/app.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, Response
 from prometheus_client import Counter
 from prometheus_client import generate_latest
+from prometheus_client import CONTENT_TYPE_LATEST
 
 app = Flask(__name__)
 
@@ -23,7 +24,11 @@ def health():
 
 @app.route("/metrics")
 def metrics():
-    return generate_latest()
+    return Response(
+        generate_latest(),
+        mimetype=CONTENT_TYPE_LATEST
+    )
+
 
 if __name__ == "__main__":
     app.run(

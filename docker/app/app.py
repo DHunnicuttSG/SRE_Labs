f20b3331550bb310c71023041451cc22372232db
dhunnicuttsg <dhunnicutt527@gmail.com>
2026-09-11 14:55:47 -0500

    frontend editing - fix datetime issue with resolved button

--- docker/app/app.py
+++ docker/app/app.py
@@ -285,7 +285,7 @@ def update_ticket(id):
         ticket.status = new_status
 
         if new_status == "RESOLVED":
-            ticket.resolved_at = datetime.now(datetime.timezone.utc)
+            ticket.resolved_at = datetime.utcnow()
 
     #
     # General updates
